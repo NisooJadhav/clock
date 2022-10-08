6242ab200a324fb09e1af64c558f2cb6e44708b4
Aarnav Pai <52203828+arnu515@users.noreply.github.com>
2022-10-08 03:55:00 +0000

    Send notification when timer gets completed

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ document.body.style.cssText = `background-image: url(https://picsum.photos/seed/
 
 //CLOCK
 function clock() {
-    const d = new Date();
-    let h = d.getHours().toString().padStart(2, '0');
-    let m = d.getMinutes().toString().padStart(2, '0');
-    let s = d.getSeconds().toString().padStart(2, '0');
-    let session='PM';
-    if(h>=12){
-        h = (h-12).toString().padStart(2, '0');
-        session='PM';
-    }
-    else{
-        session='AM';
-    }
-    document.querySelector('time').innerHTML = h + ':' + m + ':' + s;
-    document.querySelector('.ampm').innerHTML = session;
+  const d = new Date();
+  let h = d.getHours().toString().padStart(2, '0');
+  let m = d.getMinutes().toString().padStart(2, '0');
+  let s = d.getSeconds().toString().padStart(2, '0');
+  let session = 'PM';
+  if (h >= 12) {
+    h = (h - 12).toString().padStart(2, '0');
+    session = 'PM';
+  }
+  else {
+    session = 'AM';
+  }
+  document.querySelector('time').innerHTML = h + ':' + m + ':' + s;
+  document.querySelector('.ampm').innerHTML = session;
 }
 setInterval(clock, 1000);
 
@@ -31,16 +31,16 @@ updateQuote();
 
 // API -> https://github.com/lukePeavey/quotable
 async function updateQuote() {
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    if (response.ok) {
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  if (response.ok) {
     // Update DOM elements
     quote.innerText = data.content;
     cite.textContent = '~ ' + data.author + ' ~';
-    } else {
+  } else {
     quote.textContent = "An error occured";
     console.log(data);
-    }
+  }
 }
 // Timer
 const btnTimer = document.querySelector('.timer-button');
@@ -48,95 +48,102 @@ const btnClock = document.querySelector('.clock-button');
 const divClock = document.querySelector('.clock');
 const divQuote = document.querySelector('.quote');
 const divTimerDisplay = document.querySelector('.timer-display');
-btnTimer.addEventListener('click',function(){
-    divClock.classList.add('hidden');
-    divQuote.classList.add('hidden');
-    divTimerDisplay.classList.remove('hidden');
-    btnClock.classList.remove('hidden');
-    btnTimer.classList.add('hidden');
+btnTimer.addEventListener('click', function() {
+  // ask for permission to send notification
+  Notification.requestPermission().then(perm => {
+    isNotificationAllowed = perm === "granted";
+  })
+  divClock.classList.add('hidden');
+  divQuote.classList.add('hidden');
+  divTimerDisplay.classList.remove('hidden');
+  btnClock.classList.remove('hidden');
+  btnTimer.classList.add('hidden');
 })
-btnClock.addEventListener('click',function(){
-    divClock.classList.remove('hidden');
-    divQuote.classList.remove('hidden');
-    divTimerDisplay.classList.add('hidden');
-    btnClock.classList.add('hidden');
-    btnTimer.classList.remove('hidden');
+btnClock.addEventListener('click', function() {
+  divClock.classList.remove('hidden');
+  divQuote.classList.remove('hidden');
+  divTimerDisplay.classList.add('hidden');
+  btnClock.classList.add('hidden');
+  btnTimer.classList.remove('hidden');
 })
 
-let inputHour=0; 
-let inputMin=0;
-let inputSec=0;
-let time=0; //Time is seconds.
+let inputHour = 0;
+let inputMin = 0;
+let inputSec = 0;
+let time = 0; //Time is seconds.
+let isNotificationAllowed = false; // If notification is allowed.
 const btnInputHour = document.querySelector('.input-hour');
-btnInputHour.addEventListener('change',function(event){
-    if(event.target.value==="1"){
-        document.querySelector('.displayhour').innerHTML="hour"
-    }
+btnInputHour.addEventListener('change', function(event) {
+  if (event.target.value === "1") {
+    document.querySelector('.displayhour').innerHTML = "hour"
+  }
 
-    inputHour = Number(event.target.value);
-    time = time+(inputHour*3600);
+  inputHour = Number(event.target.value);
+  time = time + (inputHour * 3600);
 })
 const btnInputMin = document.querySelector('.input-min');
-btnInputMin.addEventListener('change',function(event){
-    inputMin = Number(event.target.value);
-    time = time+(inputMin*60);
+btnInputMin.addEventListener('change', function(event) {
+  inputMin = Number(event.target.value);
+  time = time + (inputMin * 60);
 })
 const btnInputSec = document.querySelector('.input-sec');
-btnInputSec.addEventListener('change',function(event){
-    inputSec = Number(event.target.value);
-    time = time+inputSec;
+btnInputSec.addEventListener('change', function(event) {
+  inputSec = Number(event.target.value);
+  time = time + inputSec;
 })
 
 let timer;
 const timerContainer = document.querySelector('.timer-container');
 const timerContainerCountdown = document.querySelector('.timer-container__countdown');
 // Creating executable function
-const timerFunc = function(){
-    const hour = String(Math.trunc(time/3600)).padStart(2,'0');
-    const min = String(Math.trunc((time/60)%60)).padStart(2,'0');
-    const sec = String(Math.trunc(time%60)).padStart(2,'0');
-    // Displaying the timer
-    timerContainerCountdown.innerText = `${hour}:${min}:${sec}`;
-    
+const timerFunc = function() {
+  const hour = String(Math.trunc(time / 3600)).padStart(2, '0');
+  const min = String(Math.trunc((time / 60) % 60)).padStart(2, '0');
+  const sec = String(Math.trunc(time % 60)).padStart(2, '0');
+  // Displaying the timer
+  timerContainerCountdown.innerText = `${hour}:${min}:${sec}`;
 
-    // Stopping the timer when it is 0 seconds.
-    if(time===0){
-        clearInterval(timer);
-        btnInputHour.value=0;
-        btnInputMin.value=0;
-        btnInputSec.value=0;
-        time=0;
-        btnStop.classList.add('hidden');
-        btnStart.classList.remove('hidden');
-        timerContainerCountdown.classList.add('hidden');
-        timerContainer.classList.remove('hidden');
-    }
-    time--;
+
+  // Stopping the timer when it is 0 seconds.
+  if (time === 0) {
+    // Show notification
+    if (isNotificationAllowed) new Notification("Timer", { body: "Time is up!", icon: "/clock.png" })
+    clearInterval(timer);
+    btnInputHour.value = 0;
+    btnInputMin.value = 0;
+    btnInputSec.value = 0;
+    time = 0;
+    btnStop.classList.add('hidden');
+    btnStart.classList.remove('hidden');
+    timerContainerCountdown.classList.add('hidden');
+    timerContainer.classList.remove('hidden');
+  }
+  time--;
 
 }
 
 const btnStart = document.querySelector('.start');
 const btnStop = document.querySelector('.stop');
-btnStart.addEventListener('click',function(){
-    if(time>0){
-        timerFunc();
-        timer = setInterval(timerFunc,1000);
-        btnStop.classList.remove('hidden');
-        btnStart.classList.add('hidden');
-        timerContainerCountdown.classList.remove('hidden');
-        timerContainer.classList.add('hidden');
-    }
-    
+btnStart.addEventListener('click', function() {
+  if (time > 0) {
+    timerFunc();
+    timer = setInterval(timerFunc, 1000);
+    btnStop.classList.remove('hidden');
+    btnStart.classList.add('hidden');
+    timerContainerCountdown.classList.remove('hidden');
+    timerContainer.classList.add('hidden');
+  }
+
 })
 const selecElement = document.querySelectorAll('.input')
-btnStop.addEventListener('click',function(){
-    clearInterval(timer);
-    btnInputHour.value=0;
-    btnInputMin.value=0;
-    btnInputSec.value=0;
-    time=0;
-    btnStop.classList.add('hidden');
-    btnStart.classList.remove('hidden');
-    timerContainerCountdown.classList.add('hidden');
-    timerContainer.classList.remove('hidden');
+btnStop.addEventListener('click', function() {
+  clearInterval(timer);
+  btnInputHour.value = 0;
+  btnInputMin.value = 0;
+  btnInputSec.value = 0;
+  time = 0;
+  btnStop.classList.add('hidden');
+  btnStart.classList.remove('hidden');
+  timerContainerCountdown.classList.add('hidden');
+  timerContainer.classList.remove('hidden');
 })
